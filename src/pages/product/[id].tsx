5e0a380b1d3816d9535d900dbfc0c348f637f472
rivnoj <84639053+rivnoj@users.noreply.github.com>
2024-07-08 11:16:38 -0300

    Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,24 @@
 import { useRouter } from 'next/router';
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product';
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt tempora, voluptatibus magnam velit ipsa harum magni assumenda vitae corporis nulla sapiente, vero corrupti porro alias laudantium tenetur nostrum commodi ut?</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
