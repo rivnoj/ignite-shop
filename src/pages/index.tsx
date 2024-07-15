3e3194e24da53ae480758f502fd288d888c294db
rivnoj <84639053+rivnoj@users.noreply.github.com>
2024-07-15 20:17:16 -0300

    Prefetch de links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home({ products }: HomeProps) {
       {
         products.map(product => {
           return (
-            <Link href={`/product/${product.id}`} key={product.id}>
+            <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
               <Product className="keen-slider__slide">
                 <Image src={product.imageUrl} width={528} height={480} alt="" />
 
