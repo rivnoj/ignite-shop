2d305fa016a04e3724cf3871b3d8f9d6db7f3fdf
rivnoj <84639053+rivnoj@users.noreply.github.com>
2024-07-17 10:44:55 -0300

    Checkout no Stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -12,14 +12,18 @@ interface ProductProps {
     imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   }
 }
 
 export default function Product({ product }: ProductProps) {
-  const { isFallback } = useRouter()
+  // const { isFallback } = useRouter()
 
-  if (isFallback) {
-    return <p>Loading...</p>
+  // if (isFallback) {
+  //   return <p>Loading...</p>
+  // }
+  function handleBuyProduct() {
+    console.log(product.defaultPriceId)
   }
 
   return (
@@ -33,7 +37,7 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>
+        <button onClick={handleBuyProduct}>
           Comprar agora
         </button>
       </ProductDetails>
@@ -73,6 +77,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           currency: 'BRL',
         }).format(price.unit_amount / 100),
         description: product.description,
+        defaultPriceId: price.id,
       }
     },
     revalidate: 60 * 60 * 1, //salvar a página 1 hora no cache
